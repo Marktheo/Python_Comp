ffbe77ce9aeeacead526f825d9dccb36e1a05b9f
Marktheo <marcosvinytheo@gmail.com>
2022-11-02 20:40:36 -0300

    Exercício 4

--- 1 - Estrutura Sequencial/04 - Média.py
+++ 1 - Estrutura Sequencial/04 - Média.py
@@ -6,4 +6,4 @@ num2 = float(input("Digite a 2ª nota: "))
 num3 = float(input("Digite a 3ª nota: "))
 num4 = float(input("Digite a 4ª nota: "))
 
-print("A média final é", media(num1, num2, num3, num4))
+print("\nA média final é", media(num1, num2, num3, num4))
