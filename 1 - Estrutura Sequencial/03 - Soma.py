f74114303087891ebbeaf4c39a16f5647b2a6d89
Marktheo <marcosvinytheo@gmail.com>
2022-11-02 20:40:07 -0300

    Exercício 3

--- 1 - Estrutura Sequencial/03 - Soma.py
+++ 1 - Estrutura Sequencial/03 - Soma.py
@@ -4,4 +4,4 @@ def soma(x, y):
 num1 = float(input("Digite um número: "))
 num2 = float(input("Digite outro número: "))
 
-print("O resultado da soma é", soma(num1, num2))
+print("\nO resultado da soma é", soma(num1, num2))
