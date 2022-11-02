b1475425245e80d853beb5b1f353cebe77fc818f
Marktheo <marcosvinytheo@gmail.com>
2022-11-02 20:20:33 -0300

    Exercício 14

---
+++ 1 - Estrutura Sequencial/14 - Pescador.py
@@ -0,0 +1,12 @@
+def excesso(k):
+    return (k - 50)
+
+
+def multa(k):
+    return (excesso(k) * 4)
+
+
+num = float(input("Informe o peso em kilos: "))
+
+print("O excesso foi de", round(excesso(num), 2), "kilos")
+print("O valor da multa é de R$", round(multa(num), 2))
