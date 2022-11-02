6d474828a20a723442ef6a294aa71b31a5a6f83f
Marktheo <marcosvinytheo@gmail.com>
2022-11-02 20:18:35 -0300

    Exercício 13

---
+++ 1 - Estrutura Sequencial/13 - Peso Ideal Completo.py
@@ -0,0 +1,16 @@
+def peso(h, s):
+
+    if(s == 'm'):
+        return ((72.7 * h) - 58)
+
+    elif(s == 'f'):
+        return ((62.1 * h) - 44.7)
+
+    else:
+        return 0
+    
+
+num = float(input("Informe sua altura em metros: "))
+sex = input("Informe seu sexo ('m' ou 'f'): ")
+
+print("Seu peso ideal é", round(peso(num, sex), 2), "Kg")
