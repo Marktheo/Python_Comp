e5bb762ec1480d736006c8f9a343289cb50fa4b7
Marktheo <marcosvinytheo@gmail.com>
2022-11-02 20:28:08 -0300

    Exercício 16

---
+++ 1 - Estrutura Sequencial/16 - Loja de Tintas.py
@@ -0,0 +1,14 @@
+import math
+
+def litros(m):
+    return (m / 3)
+
+
+def latas(m):
+    return (math.ceil(litros(m) / 18))
+
+num = float(input("Informe a área a ser pintada em metros²: "))
+
+print("Serão necessários", round(litros(num), 2), "litros de tinta")
+print("Será(ão) necessária(s)", round(latas(num), 2), "lata(s) de tinta")
+print("O valor total será de R$", round(latas(num) * 80, 2))
