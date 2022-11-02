fa98103ec503b3282fa764023483b60002d30b49
Marktheo <marcosvinytheo@gmail.com>
2022-11-02 00:56:13 -0300

    Otimizando Ex_01

--- Estrutura de Decisão/Ex_01.py
+++ Estrutura de Decisão/Ex_01.py
@@ -1,13 +1,12 @@
-#@author: Marktheo
 #https://wiki.python.org.br/EstruturaDeDecisao
 
-num1 = input("Digite um número: ")
-num2 = input("Digite outro número: ")
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número: "))
 
-if(float(num1) > float(num2)):
+if(num1 > num2):
     print("O 1º número é o maior")
 
-elif(float(num1) < float(num2)):
+elif(num1 < num2):
     print("O 2º número é o maior")
 
 else:
