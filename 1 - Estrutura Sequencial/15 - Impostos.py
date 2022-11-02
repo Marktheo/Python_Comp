267efd99a6a86e2b1b295ffd51b718074718e52f
Marktheo <marcosvinytheo@gmail.com>
2022-11-02 20:25:38 -0300

    Exercício 15

---
+++ 1 - Estrutura Sequencial/15 - Impostos.py
@@ -0,0 +1,11 @@
+def salario(l, h):
+    return (l * h)
+
+num1 = float(input("Digite seu lucro por hora: "))
+num2 = float(input("Digite sua carga horária mensal: "))
+
+print("Seu salário bruto é R$", round(salario(num1, num2), 2))
+print("IR (11%): R$", round(salario(num1, num2) * 11 / 100, 2))
+print("INSS (8%): R$", round(salario(num1, num2) * 8 / 100, 2))
+print("Sindicato (5%): R$", round(salario(num1, num2) * 5 / 100, 2))
+print("Seu salário líquido é R$", round(salario(num1, num2) * 76 / 100, 2))
