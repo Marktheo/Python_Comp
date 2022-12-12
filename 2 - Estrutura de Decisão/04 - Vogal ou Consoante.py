6a751fc3edd2f339f88123c67dfe9cf9660117a4
Marcos Theodoro <marcosvinytheo@gmail.com>
2022-12-12 04:28:53 -0300

    Exercício 4

---
+++ 2 - Estrutura de Decisão/04 - Vogal ou Consoante.py
@@ -0,0 +1,7 @@
+num = input("Digite uma letra: ")
+
+if(num == 'A' or num == 'a' or num == 'E' or num == 'e' or num == 'I' or num == 'i' or num == 'O' or num == 'o' or num == 'U' or num == 'u'):
+    print("A letra informada é uma vogal")
+
+else:
+    print("A letra informada é uma consoante")
