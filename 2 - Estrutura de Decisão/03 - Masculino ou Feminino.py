beed6e4aebb40b69d979acb9748a90debcfdc294
Marcos Theodoro <marcosvinytheo@gmail.com>
2022-12-12 04:28:44 -0300

    Exercício 3

---
+++ 2 - Estrutura de Decisão/03 - Masculino ou Feminino.py
@@ -0,0 +1,10 @@
+num = input("Digite a letra correspondente ao seu sexo: ")
+
+if(num == 'M' or num == 'm'):
+    print("Masculino")
+
+elif(num == 'F' or num == 'f'):
+    print("Feminino")
+
+else:
+    print("Sexo inválido")
